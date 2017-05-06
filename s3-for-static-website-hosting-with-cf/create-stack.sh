62b591d3139225bab5c52ec592fe3dfbd3c5fd35
Ram Gopinathan <rprakashg@outlook.com>
2017-05-06 07:24:16 -0700

    changed from s3origin to custom origin

--- s3-for-static-website-hosting-with-cf/create-stack.sh
+++ s3-for-static-website-hosting-with-cf/create-stack.sh
@@ -4,5 +4,5 @@ params=$(cat template.parameters.json | jq -r '.Parameters[] | "ParameterKey=" +
 tags=$(cat template.parameters.json | jq -r '.Tags[] | "Key=" + .Key + ",Value=" + .Value')
 
 StackName=$(cat template.parameters.json | jq -r '.StackName')
-echo 'creating stack $StackName'
+echo 'creating stack' $StackName
 aws cloudformation create-stack --stack-name $StackName --capabilities "CAPABILITY_NAMED_IAM" --region "us-west-2" --template-body file://template.json --parameters $params --profile personal --tags $tags
